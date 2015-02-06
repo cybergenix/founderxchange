9d84015990b32eea9369b803b0e9844041c1d15c
Vishak Ganti <cybergenix@gmx.com>
2015-02-06 16:11:20 +0530

    Remove scaffold.css file to reflect the changed css background styling in custom.css file.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,18 @@
 class Profile < ActiveRecord::Base
 
+#Method to store options in the form of a HashMap to include in the views form.
+	PARTNER_ROLE = { 
+0 => 'CHOOSE:',			
+1 => 'Co-Founder', 
+2 => 'Mentor',
+3 => 'Adviser',
+4 => 'Intern' 
+}
+
+#Return function to retrive object stored in the database and match it with the HashMap above.
+def return_partner_role
+    PARTNER_ROLE[id]
+  end
+
       belongs_to :user
 end
